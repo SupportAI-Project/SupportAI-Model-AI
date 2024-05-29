89a574185808e1d344a1a3a34db7e2c4390285cf
Tomer <tomermi123@gmail.com>
2024-05-29 20:46:40 +0300

    feat: Refactor OpenAIService to use conversation messages

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { OpenAIService } from './openai.service';
-import { OpenAiDTO } from "../dto/openai.dto";
 
 @Injectable()
 export class AppService {
   constructor(private openaiService: OpenAIService) {}
   
-  async getGuide(openAIDto: OpenAiDTO): Promise<string> {
-    const result = await this.openaiService.getGuide(openAIDto);
-    return 'Hello World!' + result;
-  }
 }
--- src/services/openai.service.ts
+++ src/services/openai.service.ts
@@ -1,25 +1,33 @@
 import { Injectable  } from "@nestjs/common";
 import OpenAIApi  from "openai";
-import { OpenAiDTO } from "src/dto/openai.dto";
-import { SYSTEM_MESSAGE, GPT_MODEL } from "src/lib/constans";
+import { OpenAiDTO, message } from "src/dto/openai.dto";
+import * as constants from "src/lib/openai/constants"
 
 @Injectable()
 export class OpenAIService {
     private readonly openai: OpenAIApi;
+    private readonly messagesConvo: { [key: string]: string };
     
     constructor() {
           this.openai = new OpenAIApi();
+          this.messagesConvo = constants.MESSAGES;
     }
 
+    getConversation (messagesConvo: { [key: string]: string }): OpenAiDTO {
+        const model: string = constants.GPT_MODEL;
+        const messages: message[] = Object.keys(messagesConvo).map((key, index) => {
+            const role = index % 2 === 0 ? 'user' : 'assistant';
+            const name = key.match(/(USER|REP)_MESSAGE(\d*)/)[0].toLowerCase();
+            const content = messagesConvo[key];
+            return { role, name, content };
+        });
+        return { model, messages };
+    }
+
+
     async getGuide(openAIDto: OpenAiDTO): Promise<string> {
-        // const dto: OpenAiDTO = {
-        //     model: GPT_MODEL,
-        //     messages: [
-        //         { role: 'system', content: SYSTEM_MESSAGE , name: 'system'},
-        //         { role: 'user', content: supportNote, name: 'user'}
-        //     ],
-        // };
-        const response = await this.openai.chat.completions.create(openAIDto);
+        const dTO = this.getConversation(this.messagesConvo);
+        const response = await this.openai.chat.completions.create(dTO);
         return response.choices[0].message.content;
     }
     
