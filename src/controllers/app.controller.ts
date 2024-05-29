6a262d38dc1b44036ee4e3ae4738d8afdc6dcf44
Tomer <tomermi123@gmail.com>
2024-05-29 20:45:17 +0300

    refactor: Remove unused endpoint in AppController

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,14 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../services/app.service';
-import { OpenAiDTO } from "../dto/openai.dto";
 
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async getGuide(openAIDto: OpenAiDTO): Promise<string> {
-    return await this.appService.getGuide(openAIDto);
-  }
 }
