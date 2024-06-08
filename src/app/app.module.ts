72ea2e1e2ca2e4a4c4980efdc1c3dad49910a726
Tomer <tomermi123@gmail.com>
2024-06-08 19:19:23 +0300

    fix key problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 import {OpenAIModule} from '../openai/modules/openai.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [OpenAIModule],
+  imports: [OpenAIModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
