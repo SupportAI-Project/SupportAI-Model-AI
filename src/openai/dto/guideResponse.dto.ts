86a8ac53a01d8033454f9d8065f5440e68931364
Tomer <tomermi123@gmail.com>
2024-08-14 14:53:46 +0300

    chore: Add title and contentHTML properties to GuideResponseDTO

--- src/openai/dto/guideResponse.dto.ts
+++ src/openai/dto/guideResponse.dto.ts
@@ -1,3 +1,18 @@
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class GuideResponseDTO {
-    steps: string[];
-};
+  @IsString()
+  @ApiProperty({
+    description: 'Title of the guide',
+    example: 'How to use Swagger with NestJS',
+  })
+  title: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Content of the guide in HTML format',
+    example: '<p>This is a guide on how to use Swagger with NestJS.</p>',
+  })
+  contentHTML: string;
+}
