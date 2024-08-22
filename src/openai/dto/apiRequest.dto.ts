77d9208f8b7f6dfe82a8192624efa9b400220129
Tomer <tomermi123@gmail.com>
2024-08-22 19:11:24 +0300

    refactor: Separate interfaces into different entity files for better organization

--- src/openai/dto/apiRequest.dto.ts
+++ src/openai/dto/apiRequest.dto.ts
@@ -1,5 +1,32 @@
+// if you think it is better to seperate the interfaces into different entity files, please let me know.
+export interface Message {
+    name: string;
+    content: string;
+    role: Role;
+}
+
+export interface Chat {
+    chatId: number;
+    customerId: number;
+    messages: Message[];
+}
+
+export interface User {
+    userId: number;
+    username: string;
+    roles?: Role[];
+}
+
+enum Role {
+    ASSISTANT = 'assistant',
+    USER = 'user',
+    SYSTEM = 'system',
+    FUNCTION = 'function',
+}
+
 export class apiRequestDTO {
-    _id: string;
-    chatId: string;
-    chatConversation: Record<string, any>;
+    chatId: number;
+    customerId: number;
+    user: User;
+    messages: Message[];
 }
