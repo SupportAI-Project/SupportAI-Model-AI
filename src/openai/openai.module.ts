341ff22cc1e5f745680eaec203639d1c505c828a
Tomer <tomermi123@gmail.com>
2024-08-22 19:09:54 +0300

    refactor: move openai.module to root dir of openai.

--- src/openai/openai.module.ts
+++ src/openai/openai.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { OpenAIController } from '../controllers/openai.controller';
-import { OpenAIService } from '../services/openai.service';
+import { OpenAIController } from './openai.controller';
+import { OpenAIService } from './openai.service';
 import { ConfigModule } from '@nestjs/config';
 
 
