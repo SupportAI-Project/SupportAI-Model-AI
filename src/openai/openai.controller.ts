65176c99e14871e381e579a68617e9645afb4799
Tomer <tomermi123@gmail.com>
2024-08-22 19:08:49 +0300

    refactor: Change "generateGuide" function to return GuideResponseDTO in `openai.controller`

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Post, Body } from "@nestjs/common";
-import { OpenAIService } from "../services/openai.service";
-import { apiRequestDTO } from "../dto/apiRequest.dto";
-// import { GuideResponseDTO } from "../dto/guideResponse.dto";
+import { OpenAIService } from "./openai.service";
+import { apiRequestDTO } from "./dto/apiRequest.dto";
+import { GuideResponseDTO } from "./dto/guideResponse.dto";
 
 @Controller("openai")
 export class OpenAIController {
     constructor(private readonly openaiService: OpenAIService) {}
 
     @Post("generate-guide")
-    async generateGuide(@Body() chat: apiRequestDTO): Promise<string> {
+    async generateGuide(@Body() chat: apiRequestDTO): Promise<GuideResponseDTO> {
         const guide = await this.openaiService.generateGuide(chat);
         console.log("Guide: ", guide);
         return guide;
