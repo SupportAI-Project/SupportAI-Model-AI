7d535c78b39aaa793aad60dc47c7cd0f69bba05e
Tomer <tomermi123@gmail.com>
2024-08-22 19:12:39 +0300

    Feat: Refactor OpenAI Service to format incoming chat, generate a guide using the API, and return a formatted guide.

---
+++ src/openai/openai.service.ts
@@ -0,0 +1,88 @@
+import { Injectable, Logger  } from "@nestjs/common";
+import OpenAIApi  from "openai";
+import { apiRequestDTO, Message } from "./dto/apiRequest.dto";
+import { GuideResponseDTO} from "./dto/guideResponse.dto";
+import {
+    GPT_MODEL, 
+    SYSTEM_MESSAGE, 
+    USER_PROMPT, 
+    OUTPUT_INDICATOR, 
+    CONTEXT_EXAMPLE, 
+} from "./constants/constants";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class OpenAIService {
+    private readonly openai: OpenAIApi;
+    private readonly configService: ConfigService;
+    
+    constructor() {
+        this.configService = new ConfigService();
+        this.openai = new OpenAIApi({ apiKey: this.configService.get('OPENAI_API_KEY') });
+    }
+
+
+    formatConversation (requestDto: apiRequestDTO): Message[] {
+        console.log("formatConversation: requestDto: ", requestDto);
+        const chatId = requestDto.chatId;
+        const customerId = requestDto.user.userId;
+        const userName = requestDto.user.username;
+        const role = requestDto.user.roles[0];
+        const messages = requestDto.messages;
+
+        // console.log("role: ", role);
+
+        if (!messages) {
+            throw new Error('No messages found in conversation');
+        }
+
+        const formattedMessages = messages.map(message => {
+            // console.log("message.content: ", message.content);
+            const { content } = message;
+            return {
+                name: userName,
+                role: role,
+                content: content,
+            };
+        });
+        Logger.log("formattedMessages: ", formattedMessages);
+        if (!formattedMessages) {
+            throw new Error('Error formatted messages');
+        }
+        return formattedMessages;
+    };
+
+    async generateGuide(requestDto: apiRequestDTO): Promise<GuideResponseDTO> {
+
+        // console.log("(AI) - generateGuide: requestDto: ", requestDto);
+        // console.log("messages: ", chat);
+        
+        const messages = this.formatConversation(requestDto);
+        // console.log("messages: ", messages);
+        Logger.log('Generating guide...');
+        const response = await this.openai.chat.completions.create({
+            model: GPT_MODEL,
+            messages: [
+                { role: 'system', content: SYSTEM_MESSAGE },
+                { role: 'user', content: USER_PROMPT },
+                { role: 'system', content: OUTPUT_INDICATOR },
+                { role: 'user', content: CONTEXT_EXAMPLE },
+                ...messages,
+            ],
+        });
+        // Logger.log('Guide generated: ', response.choices[0].message.content);
+        const guide = this.formatGuide(response.choices[0].message.content);
+        return guide;
+    };
+
+    formatGuide(guide: string): GuideResponseDTO {
+        const lines = guide.split('\n');
+        const title = lines.shift() || '';
+        const formatedGuide: GuideResponseDTO = {
+            title: title,
+            contentHTML: lines.join('\n'),
+        };
+        console.log("formatedGuide: ", formatedGuide);
+        return formatedGuide;
+    };
+};
